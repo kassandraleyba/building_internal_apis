6bb530896fdc80dc5d37bcf7e7a3d15a5f0becee
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-13 20:16:54 -0600

    Start first api request test

---
+++ spec/requests/api/v1/books_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Books API" do
+  it "sends a list of books" do
+    create_list(:book, 3)
+
+    get 'api/v1/books'
+
+    expect(response).to be_successful
+  end
+end
