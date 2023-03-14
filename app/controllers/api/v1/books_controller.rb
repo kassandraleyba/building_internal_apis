bf6454503565b03a4075de4cb806f3287b5d13e3
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-13 22:18:06 -0600

    Add passing test1*

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    
+    render json: Book.all
   end
 end
--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -5,7 +5,31 @@ describe "Books API" do
     create_list(:book, 3)
 
     get '/api/v1/books'
-
+   
     expect(response).to be_successful
+
+    books = JSON.parse(response.body, symbolize_names: true)
+
+    expect(books.count).to eq(3)
+
+    books.each do |book|
+      expect(book).to have_key(:id)
+      expect(book[:id]).to be_an(Integer)
+
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a(String)
+
+      expect(book).to have_key(:author)
+      expect(book[:author]).to be_a(String)
+
+      expect(book).to have_key(:genre)
+      expect(book[:genre]).to be_a(String)
+
+      expect(book).to have_key(:summary)
+      expect(book[:summary]).to be_a(String)
+
+      expect(book).to have_key(:number_sold)
+      expect(book[:number_sold]).to be_an(Integer)
+    end
   end
 end
